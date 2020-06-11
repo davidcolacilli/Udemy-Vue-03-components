02def4a878a24a9e373342d55ce0a8c2a8dd872a
Colacilli <davidcolacilli@live.com>
2020-06-11 16:35:37 -0300

    Posts

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app-container" class="container">
+    <header class="header">
+      <h1>Posts</h1>
+    </header>
+    <Posts></Posts>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Posts from './components/Posts.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Posts
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#app-container {
   margin-top: 60px;
+}
+h1 {
+  text-align: center;
+  margin-bottom: 1.5rem;
 }
 </style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,50 @@
+<template>
+    <table class="table table-dark">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">User</th>
+                <th scope="col">Message</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="post in posts" :key="post.id">
+                <th scope="row">{{post.id}}</th>
+                <td>{{post.user}}</td>
+                <td>{{post.message}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+<script>
+
+export default {
+    name: "Posts",
+    props: {},
+    data() {
+        return {
+            posts: [
+                {
+                    id: 1,
+                    user: "Juan",
+                    message: "First kick"
+                },
+                {
+                    id: 2,
+                    user: "Pedro",
+                    message: "Ouch!"
+                },
+                {
+                    id: 3,
+                    user: "Chuck",
+                    message: "There's no third kick ;)"
+                }
+
+            ]
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
